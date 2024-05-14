2866748d55ada3e2df8c1ab88bdb8bfcc93e5d6e
AndreyCorpse28090 <280990corpse@gmail.com>
2024-05-14 21:46:26 +0300

    d

--- serialization_Bukov/People.cs
+++ serialization_Bukov/People.cs
@@ -1,12 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace serialization_Bukov
 {
     [Serializable]
-    public class Person
+    public class People
     {
-        public string Name { get; set; }
+        public List<Person> Persons { get; set; }
+
+        // JSON формат
+        public static void SerializationJson(People people, string path)
+        {
+            string json = JsonConvert.SerializeObject(people, Formatting.Indented);
+            File.WriteAllText(path, json);
+            Console.WriteLine("Объект записан в JSON файл.");
+        }
+
+        public static People DeserializationJson(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<People>(json);
+        }
+
+        // Бинарный формат
+        public static void SerializationBinary(People people, string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, people);
+            }
+            Console.WriteLine("\n\nОбъект записан в бинарный файл.");
+        }
+
+        public static People DeserializationBinary(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (People)formatter.Deserialize(stream);
+            }
+        }
+
+        // XML формат
+        public static void XmlSerialization(People people, string path)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(People));
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(stream, people);
+                Console.WriteLine("\n\nОбъект записан в XML файл.");
+            }
+        }
+
+        public static People XmlDeserialization(string path)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(People));
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                return (People)xmlSerializer.Deserialize(stream);
+            }
+        }
         
-        
+        //Soap формат
+        public static void SoapSerialization(People people, string path)
+        {
+            SoapFormatter formatter = new SoapFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(stream, people);
+                Console.WriteLine("\n\nОбъект записан в Soap файл.");
+            }
+        }
+
+        public static People SoapDeserialization(string path)
+        {
+            SoapFormatter formatter = new SoapFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                return (People)formatter.Deserialize(stream);
+            }
+        }
+
+        [Serializable]
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+        }
     }
 }
--- serialization_Bukov/Program.cs
+++ serialization_Bukov/Program.cs
@@ -1,9 +1,77 @@
-﻿namespace serialization_Bukov
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace serialization_Bukov
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            string pathJson = @"E:\programming\Visual Studio\repos\serialization_Bukov\serialization_Bukov\PersonJson.txt";
+            string pathBinary = @"E:\programming\Visual Studio\repos\serialization_Bukov\serialization_Bukov\PersonBinary.txt";
+            string pathXml = @"E:\programming\Visual Studio\repos\serialization_Bukov\serialization_Bukov\PersonXml.txt";
+            string pathSoap = @"E:\programming\Visual Studio\repos\serialization_Bukov\serialization_Bukov\PersonSoap.txt";
+            People people = new People();
+
+            people.Persons = new List<People.Person>
+            {
+                new People.Person() { Name = "John", Age = 20, Gender = "Male" },
+                new People.Person() { Name = "Mary", Age = 21, Gender = "Female" },
+                new People.Person() { Name = "Peter", Age = 22, Gender = "Male" }
+            };
+
+            // JSON формат
+            People.SerializationJson(people, pathJson);
+            Console.WriteLine("Недесериализованные данные из JSON файла: ");
+            Console.WriteLine(File.ReadAllText(pathJson));
+
+            People deserializedPeople = People.DeserializationJson(pathJson);
+
+            Console.WriteLine("Десериализованные данные из JSON файла: ");
+            foreach (var person in deserializedPeople.Persons)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Gender: {person.Gender}");
+            }
+
+            // Бинарный формат
+            People.SerializationBinary(people, pathBinary);
+            Console.WriteLine("Недесериализованные данные из бинарного файла: ");
+            Console.WriteLine(File.ReadAllText(pathBinary));
+            People binaryDeserialized = People.DeserializationBinary(pathBinary);
+
+            Console.WriteLine("Десериализованные данные из бинарного файла: ");
+            foreach (var person in binaryDeserialized.Persons)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Gender: {person.Gender}");
+            }
+
+            // XML формат
+            People.XmlSerialization(people, pathXml);
+            Console.WriteLine("Недесериализованные данные из XML файла: ");
+            Console.WriteLine(File.ReadAllText(pathXml));
+            People xmlDeserialized = People.XmlDeserialization(pathXml);
+
+            Console.WriteLine("Десериализованные данные из XML файла: ");
+            foreach (var person in xmlDeserialized.Persons)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Gender: {person.Gender}");
+            }
+            
+            //Soap формат
+            People.SoapSerialization(people, pathSoap);
+            Console.WriteLine("Недесериализованные данные из Soap файла: ");
+            Console.WriteLine(File.ReadAllText(pathSoap));
+            People.SoapDeserialization(pathSoap);
+            People soapDeserialized = People.SoapDeserialization(pathSoap);
+            Console.WriteLine("Десериализованные данные из Soap файла: ");
+            foreach (var person in soapDeserialized.Persons)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Gender: {person.Gender}");
+            }
+        }
     }
-  }
 }
+    
